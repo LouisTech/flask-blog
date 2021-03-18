a5b66c5e7e24f2dc443690c1111b43269ec4883b
LouisTech <louis.dan@hotmail.co.uk>
2021-03-18 00:33:37 +0000

    home and posts page plus visual overhaul

--- blog/routes.py
+++ blog/routes.py
@@ -9,39 +9,65 @@ from flask_login import current_user, login_user, logout_user
 @app.route("/")
 @app.route("/home", methods=['GET', 'POST'])
 def home():
-    form = SearchForm(request.form)
+    # Get first four posts latest first
+    posts = Post.query.all()
+    four_posts = []
+    for i in range(len(posts)-1,len(posts)-5,-1):
+        four_posts.append(posts[i])
+    search_form = SearchForm(request.form)
     if request.method == 'POST':
-        searched = form.search.data
+        searched = search_form.search.data
+        return redirect(url_for('posts', search_form = searched))
+    return render_template('home.html', title='LT | Home', posts=four_posts, search_form=search_form)
+
+@app.route("/posts", methods=['GET', 'POST'])
+def posts():
+    posts = Post.query.all()
+    search_form = SearchForm(request.form)
+    if request.method == 'POST':
+        searched = search_form.search.data
         posts = Post.query.filter(Post.title.contains(searched) | Post.content.contains(searched))
     else:
         posts = Post.query.all()
-    return render_template('home.html', title='Home', posts=posts, search_form=form)
+    #format post dates
+    return render_template('posts.html', title='LT | Posts', posts=posts, search_form=search_form)
 
 
-@app.route("/about")
+@app.route("/about", methods=['GET', 'POST'])
 def about():
-    return render_template('about.html', title='About')
+    search_form = SearchForm(request.form)
+    if request.method == 'POST':
+        searched = search_form.search.data
+        return redirect(url_for('posts', search_form=searched))
+    return render_template('about.html', title='LT | About', search_form=search_form)
 
-@app.route("/post/<int:post_id>")
+@app.route("/post/<int:post_id>", methods=['GET', 'POST'])
 def post(post_id):
+    search_form = SearchForm(request.form)
+    if request.method == 'POST':
+        searched = search_form.search.data
+        return redirect(url_for('posts', search_form=searched))
+
     post = Post.query.get_or_404(post_id)
     comments = Comment.query.filter(Comment.post_id == post.id)
     comment_form = CommentForm()
     rating_form = RatingForm()
     #Calculate average rating
     avg_rating = 0.0
-    user_rating = Rating.query.filter_by(rater_id=current_user.id, post_id=post.id).first()
-    if not user_rating:
-        user_rating = "Not Rated"
-    else:
-        user_rating = str(user_rating.rating)
-        
+    user_rating=None
+    if current_user.is_authenticated:
+        user_rating = Rating.query.filter_by(rater_id=current_user.id, post_id=post.id).first()
+        if not user_rating:
+            user_rating = "Not Rated"
+        else:
+            user_rating = str(user_rating.rating)
+
     all_ratings = Rating.query.filter_by(post_id=post.id).all()
     if all_ratings:
         for i in range(len(all_ratings)):
             avg_rating += all_ratings[i].rating
         avg_rating /= len(all_ratings)
-    return render_template('post.html', title=post.title, post=post, comments=comments, comment_form=comment_form, rating_form=rating_form, avg_rating=avg_rating, user_rating=user_rating)
+    return render_template('post.html', title=post.title, search_form=search_form, post=post, comments=comments, comment_form=comment_form, rating_form=rating_form, avg_rating=avg_rating, user_rating=user_rating)
 
 @app.route('/post/<int:post_id>/comment', methods=['GET', 'POST'])
 @login_required
@@ -66,12 +92,10 @@ def rate_post(post_id):
     comments = Comment.query.filter(Comment.post_id == post.id)
 
     rating_form = RatingForm()
-    print("user id: ", current_user.id, flush=True)
     if rating_form.validate_on_submit():
         
         #Check if user has already given this post a rating
         old_rating = Rating.query.filter_by(rater_id=current_user.id, post_id=post.id).first()
-        print("Results", old_rating, flush=True)
         if old_rating:
             old_rating.rating = rating_form.rating.data
         else:
@@ -83,6 +107,11 @@ def rate_post(post_id):
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    search_form = SearchForm(request.form)
+    if request.method == 'POST':
+        searched = search_form.search.data
+        return redirect(url_for('posts', search_form=searched))
+
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, first_name=form.first_name.data,
@@ -90,18 +119,25 @@ def register():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title='LT | Register', form=form, search_form=search_form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user)
             return redirect(url_for('home'))
-    flash('Invalid email address or password.')
-    return render_template('login.html', title='Login', form=form)
+        else:
+            flash('Invalid email address or password.')
+    
+    search_form = SearchForm(request.form)
+    if request.method == 'POST':
+        searched = search_form.search.data
+        return redirect(url_for('posts', search_form = searched))
+    return render_template('login.html', title='LT | Login', form=form, search_form=search_form)
 
 #This page needs testing!
 @app.route("/logout")
